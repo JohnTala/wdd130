1b420d3d22d9d91235e091349b129afa3c6df5fd
buy-st <stalatala@byupathway.edu>
2025-03-19 11:00:02 +0200

    added an overlay.html

---
+++ cs1106/my_books.py
@@ -0,0 +1,60 @@
+# import time
+# # with open('books.txt') as mybooks:
+     
+# #      for book in mybooks:
+# #         #   print(book)
+# #         #   time.sleep(1)
+# #           the_book=book.strip()
+# #           print(the_book)
+# #           time.sleep(1)
+
+        
+#     #    FIND THE MOST EXPENSIVE ITEM AND ITS PRICE
+# # shopping_cart = [
+# #     ["Chips", 2.99],
+# #     ["Bread", 2.50],
+# #     ["Milk", 3.19],
+# #     ["Ice Cream", 6.99],
+# #     ["Cheese", 5.99],
+# #     ["Candy bar", 0.99]
+# # ]
+# # max_price=-1
+# # expensive_item=''
+# # for item in shopping_cart:
+# #     print(item)
+#     # mostvalue_product=item[0] #the 1st thing in item list wchich is product i.e bread or milk etc
+#     # price=item[1] #the 2nd thing in item list wich is price
+#     # if price > max_price:
+#     #     max_price=price
+#     #     expensive_item=mostvalue_product
+
+# # print(f'The most expensive price is {max_price} and most expensive item is {expensive_item}')
+# # line = "     text"
+
+# # line.strip()
+
+# # print(line)
+
+# import numpy as np
+
+
+
+# Create a list of numbers
+
+numbers = [1, 2, 3, 4, 5]
+
+
+
+# Calculate the sum of the numbers
+
+sum_of_numbers = sum(numbers)
+
+
+
+# Calculate the average of the numbers
+
+average = sum_of_numbers / len(numbers)
+
+
+
+print("The average of the numbers is:", average)
